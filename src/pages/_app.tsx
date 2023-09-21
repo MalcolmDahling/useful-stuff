7d81f75580b5ab699b38f1b42d0c701d218363ee
MalcolmDahling <mac.dahling@gmail.com>
2023-09-21 21:06:36 +0200

    title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,15 @@ import { RecoilRoot } from 'recoil';
 import '@/styles/global.css';
 import Layout from '@/components/Layout/Layout';
 import '@/styles/globalStyle.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Head>
+        <title>Useful Stuff</title>
+      </Head>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
